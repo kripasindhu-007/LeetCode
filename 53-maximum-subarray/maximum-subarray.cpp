eeeb221674a62366d52eb38089bc53fd814113a3
Kripa Sindhu <96479494+kripasindhu-007@users.noreply.github.com>
2023-07-19 18:55:31 +0530

    Time: 131 ms (31.60%) | Memory: 67.6 MB (81.46%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int maxSum=INT_MIN;
+        int sum=0;
+        
+        for(auto it:nums){
+            
+            sum+=it;
+            
+           maxSum=max(sum,maxSum);
+            
+            if(sum<0) sum=0;
+        }
+        
+        return maxSum;
+        
+    }
+};
